f8a09a753b1cdd6d89279745bd5ac8fc71bd1378
pain <anatole.pain@outlook.fr>
2019-09-30 13:00:56 +0000

    modification programme menssion

--- APL1.1/TP05/exercice04.c
+++ APL1.1/TP05/exercice04.c
@@ -4,30 +4,31 @@
 int main(int argc, char * argv[]){
 
 double moyenne;
-char mention[] = "erreur01";
+char *mention = "erreur01\n";
 
 printf("Entrer votre moyenne au Bac: ");
-scanf("%d",&moyenne);
+scanf("%lf",&moyenne);
+//printf("%lf",moyenne);
 
 if(0.00 <= moyenne && moyenne < 8.00){
-	mention[] = "refusé\n";
+	mention = "refusé\n";
 }else if(8.00 <= moyenne && moyenne < 10.00){
-	mention[] = "rattrapage\n";
+	mention = "rattrapage\n";
 }else if(10.00 <= moyenne && moyenne < 12.00){
-	mention[] = "passable\n";
+	mention = "passable\n";
 }else if(12.00 <= moyenne && moyenne < 14.00){
-	mention[] = "assez bie\n";
+	mention = "assez bie\n";
 }else if(14.00 <= moyenne && moyenne < 16.00){
-	mention[] = "bien\n";
+	mention = "bien\n";
 }else if(16.00 <= moyenne && moyenne < 18.00){
-	mention[] = "Très bien\n";
+	mention = "Très bien\n";
 }else if(18.00 <= moyenne && moyenne < 20.00){
-	mention[] = "Très bien avec les félicitations du jury\n";
+	mention = "Très bien avec les félicitations du jury\n";
 }else{
- mention = "erreur02";
+ mention = "erreur02\n";
 }                                                                                                                                     
                                                
-printf("vous avez obtenus la mention %s",mention[]);
+printf("vous avez obtenus la mention %s",mention);
 
 return EXIT_SUCCESS;
 }
