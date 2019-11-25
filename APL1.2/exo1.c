039e1f63c0006dc286335180fc3fdb6dae365c0b
pain <anatole.pain@outook.fr>
2019-11-25 23:42:31 +0100

    test

---
+++ APL1.2/exo1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc, char * argv[]){
+
+    int nb_reel = 
+
+    printf("Combien de réel allez vous rentrez ?")
+    scanf("%d",nb_reel)
+
+
+
+
+
+
+
+
+
+
+
+
+    return EXIT_SUCCESS;
+}
