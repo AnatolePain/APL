565d9a42e0ef403d9b5a406c5d50fbdfdabf60d6
pain <anatole.pain@outook.fr>
2019-11-26 01:19:48 +0100

    Début TP1 APL1.1 25/11/2019

--- APL1.2/exo1.c
+++ APL1.2/exo1.c
@@ -1,21 +1,43 @@
 #include<stdio.h>
 #include<stdlib.h>
+
 int main(int argc, char * argv[]){
 
-    int nb_reel = 
+    int i; int k;
+    int indicateur;
+    int nb_de_reel = 0;
+    float *reel = NULL;
 
-    printf("Combien de réel allez vous rentrez ?")
-    scanf("%d",nb_reel)
+    /*reel = malloc(nb_de_reel*sizeof(float));*/
 
+    reel = (float*)malloc(nb_de_reel*sizeof(float));
+    printf("Combien de reel allez vous rentrez ? ");
+    scanf("%d",&nb_de_reel);
 
+    for(i = 0, k = 1; i < nb_de_reel ; i++ , k++){
+        printf("reel %d: ",k);
+        scanf("%f",&reel[i]);
+    }
 
+    for(i = 0 ; i < nb_de_reel ; i++){
 
+        indicateur = 0;
 
+        for(k = 0 ; k < nb_de_reel ; k++){
+            if(reel[i] == reel[k] ){
+                indicateur = 1;
+                printf("test %d \n",indicateur);
+            }
 
+        }
 
+        if(indicateur != 1){
+            printf("%f ,",reel[i]);
+        }else{
+             printf("test,");
+        }
 
-
-
+    }
 
 
     return EXIT_SUCCESS;
