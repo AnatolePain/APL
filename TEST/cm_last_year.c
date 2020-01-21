f7c60504e4efea3e50fd48de7f210c61e41241de
pain <anatole.pain@outook.fr>
2020-01-22 00:34:49 +0100

    modif cm de l'anné dernière

--- TEST/cm_last_year.c
+++ TEST/cm_last_year.c
@@ -1,73 +1,82 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include<string.h>
 
 #define MAX_ARG 6
 #define NB_REPETE 1
-  
-
-void testParam(int* taille, int* max, int* min){
-
-    int i;
-    int totaleArg = argc;
-
-    if(totaleArg > MAX_ARG){
-        fprintf(stderr, "erreur: trop d'arguments");
-    }else if (totaleArg != 0){
-        for (i = 1; i < totaleArg + 1; i++){
-            if(argv[i] == '-t'){
-                *taille = strtol(argv[i++]);
-            }
-
-            if(argv[i] == '-m'){
-                *max = strtol(argv[i++]);
-            }
-
-            if(argv[i] == '-M'){
-                *min = strtol(argv[i++]);
-            }
-        }
-
-        if =(!(min <= max) || !(max-min >= taille)){
-            fprintf(stderr; "Les paramètres ne sont pas cohérant");
-        }
-
-    }  
-}
 
 int tireHasard(int min, int max){
-   return (rand()%(max-min)) + min;	
+   int x = rand()%((max-min)) + min;	
+   printf("TEST01: %d\n",x);
+   return x;
 }
 
 void verifieTab(int taille, int min, int max,int *tab){
 
+    int indicateur = 0;
     int valRand;
-    int i, k;
+    int i = 0;
+    int k = 0;
 
-    for(i = 0 ; i < taille ; i++){
+   /* for(i = 0 ; i < taille; i++){
 
+        printf("\nTEST 403 tab[i] = %d\n",tab[i]);
         valRand = tireHasard(min, max);
 
-        for(k = 0 ; k < i ; k++){
-            if(valRand == tab[k] ){
-                indicateur == NB_REPETE;
+        for(k = 0 ; k < i; k++){
+            if(valRand == tab[k]){
+                printf("TEST 404 valRand = %d tab[k] == %d \n",valRand, tab[k]);
+                indicateur = NB_REPETE;
+                printf("indicateur == %d\n", indicateur);
             }
 
         }
 
-        if(indicateur == NB_REPETE){
-            *tab[i] = valRand;
+        if(indicateur != NB_REPETE){
+            tab[i] = valRand;
+            printf("NON NON NON\n");
+            indicateur = 0;
         }
+
+        printf("TEST 405 tab[i] = %d\n",tab[i]);
+
+    }*/
+
+    printf("\ni = %d, taille = %d, indicateur = %d", i, taille, indicateur);
+
+    while(i < taille){
+
+        printf("\nTEST 403 tab[i] = %d\n",tab[i]);
+        valRand = tireHasard(min, max);
+
+        for(k = 0 ; k < i; k++){
+            if(valRand == tab[k]){
+                printf("TEST 404 valRand = %d tab[k] == %d \n",valRand, tab[k]);
+                indicateur = NB_REPETE;
+                printf("indicateur == %d\n", indicateur);
+            }
+
+        }
+
+        if(indicateur != NB_REPETE){
+            tab[i] = valRand;
+            printf("NON NON NON\n");
+            indicateur = 0;
+            i++;
+        }
+
+        printf("TEST 405 tab[i] = %d\n",tab[i]);
 
     }
 
 }
 
-void afficherTab(int *tab, int taille){
+void afficherTab(int *tab,int taille){
 
     int i;
     for(i = 0 ; i < taille ; i++){
-    printf("%d ",*tab[i]);
+    printf("%d ",tab[i]);
     }
     printf("\n");
 
@@ -82,10 +91,40 @@ int main(int argc, char * argv[]){
     int *tab = NULL;
     srand(time(NULL));
 
-    testParam(&taille,&max,&min);
-    tab = (float*)malloc(taille*sizeof(int));
-    verifieTab(taille, min, max,&tab);
-    afficherTab(*tab,taille);
+    int i;
+    int totaleArg = argc;
+
+    if(totaleArg >= MAX_ARG + 2){
+        fprintf(stderr, "erreur: trop d'arguments");
+        return  EXIT_FAILURE;
+    }else if (totaleArg != 0){
+        for (i = 1; i < totaleArg; i=i+2){
+            printf("TEST 04 i = %d , %s\n", i, argv[i]);
+            if(strcmp(argv[i], "-t") == 0){
+                printf("TEST 05\n");
+                taille = (int)strtol(argv[i+1], NULL, 10);
+                printf("TEST 06, taille = %d\n", taille);
+            }else if(strcmp(argv[i], "-M") == 0){
+                 printf("TEST 07\n");
+                max = (int)strtol(argv[i+1], NULL, 10);
+                printf("TEST 08, max = %d\n", max);
+            }else if(strcmp(argv[i], "-m") == 0){
+                printf("TEST 09\n");
+                min = (int)strtol(argv[i+1], NULL, 10);
+                printf("TEST 10, min = %d\n", max);
+            }
+        }
+
+        if (!(min <= max) || !(max-min >= taille)){
+            fprintf(stderr, "Les paramètres ne sont pas cohérant");
+        }
+
+    }  
+
+    tab = (int*)malloc(taille*sizeof(int));
+    verifieTab(taille, min, max,tab);
+    printf("\n\n");
+    afficherTab(tab,taille);
 
     return EXIT_SUCCESS;
 }
