422995716b7fbb432f12fd34828124cf5106dd40
pain <anatole.pain@outook.fr>
2020-01-31 02:25:58 +0100

    fin TP02
    Signed-off-by: pain <anatole.pain@outook.fr>

--- APL2.1/TP02/exo2/Date.java
+++ APL2.1/TP02/exo2/Date.java
@@ -7,15 +7,53 @@ public class Date {
   // autre méthode
   public String toStringUp() {
 
-   int mois29 = 29;
-   int mois30 = 30;
-   int mois31 = 31;
+    int typeMois = -1;
+    int maxMois = -1;
 
+    //Verifie quel tupe de mois l'on est: 29 , 30 ou 31
+    if(this.mm == 2){
+      typeMois = 3;
 
+    }else if(this.mm <= 7){
+      typeMois = this.mm%2;
 
+    }else if(this.mm > 7){
+      this.mm -= 7;
+      typeMois = this.mm%2;
+      this.mm += 7;
+    }else{
+      System.out.println("erreur 01");
+    }
 
+    switch (typeMois) {
+      case 1:
+        maxMois = 31;
+        break;
+      case 0:
+        maxMois = 30;
+        break;
+      case 3:
+        maxMois = 29;
+        break;
+      default:
+        System.out.println("erreur dans le switch case : class Date");
+        break;
+    }
 
-   return String.format("%02d-%02d-%04d", this.jj,this.mm,this.aaaa);
+    if(this.jj == maxMois){
+      this.mm++;
+      this.jj = 1;
+    }else{
+      this.jj++;    //journée normale
+    }
+
+    if(this.mm == 13){
+      this.aaaa++;
+      this.jj = 1;
+      this.mm = 1;
+    }
+
+    return String.format("%02d-%02d-%04d", this.jj,this.mm,this.aaaa);
   }
 
   public String toString() {
@@ -24,12 +62,12 @@ public class Date {
 
   public Date(int jour, int mois, int annee) {
     this.jj = jour;
-   this.mm = mois;
-   this.aaaa = annee;
+    this.mm = mois;
+    this.aaaa = annee;
   }
 
   public Date() {
-    this.jj = 0;
+   this.jj = 0;
    this.mm = 0;
    this.aaaa = 0;
   }
--- APL2.1/TP02/exo2/Exo2.java
+++ APL2.1/TP02/exo2/Exo2.java
@@ -2,9 +2,9 @@ public class Exo2{
 
 	public static void main(String[] args) {
 			
-		Date c = new Date(28,01,2020);
+		Date c = new Date(31,12,2020);
 		System.out.println(c.toString());
 
-
+		System.out.println(c.toStringUp());
 	}
 }
