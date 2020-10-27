826aa1d840486c4ee5b31f4b44b7a5038b7225fb
pain <anatole.pain@outlook.fr>
2020-10-27 16:35:49 +0000

    ajout tp 1 APL3.3

--- APL3.1/TP01/Exo2/Tableaux.java
+++ APL3.1/TP01/Exo2/Tableaux.java
@@ -3,8 +3,20 @@ import java.util.*;
 
 public class Tableaux {
 
+
 	public static void main(String args[]) {
-		ArrayList<String> tabArgument = new ArrayList<String>();
+
+		System.out.println("1.");
 		System.out.println(Arrays.toString(args));
+		System.out.println();
+
+		System.out.println("2.");
+		String tabString[];
+		tabString = Arrays.copyOf(args,5);
+		System.out.println(Arrays.toString(tabString));
+		System.out.println();
+
+		
+
 	}
 }
