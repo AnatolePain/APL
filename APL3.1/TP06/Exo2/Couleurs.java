5f1bea76468f02eb93047e8198a9b59de3b9602f
pain <anatole.pain@outlook.fr>
2020-12-09 17:32:23 +0000

    maj

--- APL3.1/TP06/Exo2/Couleurs.java
+++ APL3.1/TP06/Exo2/Couleurs.java
@@ -5,69 +5,46 @@ import java.util.*;
 public class Couleurs extends JFrame {
 
 
-	private int numImage;
-	private JLabel image;
-	private ChangeImage observateur;
-	private  int nbImage = 5;
-
-
+	private Souris observateur;
+	private ListModel<Object> index;
+	private JPanel panneauCouleur;
+	private Lecteur l;
 
 	public Couleurs(){
 
-		//on configure la fenetre
-	    this.setSize(600, 1050);
+	    this.setSize(600, 400);
 	    this.setLocation(400, 300);
 	    this.setTitle("Couleurs");
 	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		GridLayout gestionnaire = new GridLayout(0, 2);
 		this.setLayout(gestionnaire);
+		this.panneauCouleur = new JPanel();
 
-		JButton test = new JButton("test");
- 		Lecteur l = new Lecteur();
- 		//Map<String,int[]> rgbDictionnaire = l.getDictionnaire();
+		//Lecteur
+ 		this.l = new Lecteur();
  		Set<String> idKey = l.getDictionnaire().keySet();
  		Object[] idString = idKey.toArray();
+
+ 		//JList
 		JList<Object> menu = new JList<>(idString);
 		menu.setFont(new Font("", Font.PLAIN, 20));
-		menu.addListSelectionListener(new ChangeImage());
+		menu.addListSelectionListener(new Souris(this));
+		this.index = menu.getModel();
 
- 		this.add(menu);
- 		this.add(test);
+		//ScrollPane
+		JScrollPane barDeroulante = new JScrollPane(menu);
+		barDeroulante.setVerticalScrollBar(new JScrollBar());
 
-	}
-
-
-	public void modifImage(int getX){
-
-
-		// this.remove(image);
-	 //    this.droiteOuGauche(getX);
-	 //    String nomImage = ""+numImage+".jpg";
-	 //    System.out.println("numImage = "+ nomImage);
-	 //    image = new JLabel(new ImageIcon(nomImage));
-	 //    image.addMouseListener(observateur);
-	 //    image.setHorizontalAlignment(JLabel.CENTER);
-	 //    this.add(image, BorderLayout.CENTER); 
-	 //    image.revalidate();
+ 		this.add(barDeroulante);
+ 		this.add(panneauCouleur);
 
 	}
 
 
-	public void droiteOuGauche(int x){
-
-		// if(x < 250){
-		// 	numImage--;
-		// }else if(x > 250){
-		// 	numImage++;
-		// }
-
-		// if(numImage == nbImage){
-	 //    	numImage = 1;
-	 //    }else if (numImage == 0){
-	 //    	numImage = 4;
-	 //    }
-
+	public void changeCouleur(int num){
+		Color rgb = l.getDictionnaire().get(this.index.getElementAt(num));
+		this.panneauCouleur.setBackground(rgb);
 	}
 
 
--- APL3.1/TP06/Exo2/Main.java
+++ APL3.1/TP06/Exo2/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Couleurs g = new Couleurs();
-		g.setVisible(true);
+		Couleurs c = new Couleurs();
+		c.setVisible(true);
 	}
 
 }
